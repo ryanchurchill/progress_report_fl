2a619cdb6a21502393edf9f4581d7a04f9eae4b5
ryanchurchill <ryan.churchill@gmail.com>
2023-05-14 19:34:41 -0700

    super basic navigation tab

---
+++ lib/tests_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class TestsPage extends StatelessWidget
+{
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text("Tests Page")
+        ]
+      )
+    );
+  }
+}
